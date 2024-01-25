f15e0ebf9cf1af648e5b3ab95fecf72de7560ccd
ephraim.rogers <ephraim.rogers@consilium.technology>
2024-01-25 15:42:59 +1030

    feat: add lexer and token

---
+++ go.mod
@@ -0,0 +1,3 @@
+module zag
+
+go 1.21.1
---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // variables i.e. x, res etc
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
